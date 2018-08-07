fcb2fbe56b58bdcf3ef3aa96e9894c5d9fd87f6e
Jain <rajajain@lm-maa-22004953.paypalcorp.com>
2018-08-07 09:02:12 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+    public static String TreeMapToString(TreeMap<Integer,Integer> poly){
+
+    }
+
+    public static TreeMap<Integer,Integer> StringToTreeMap(String poly){
+
+    }
+    public static void main(String[] args){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter 1 st polynomial:");
+        String A = scanner.nextLine();
+        System.out.println("Enter 2 nd polynomial:");
+        String B = scanner.nextLine();
+        
+
+    }
+}
